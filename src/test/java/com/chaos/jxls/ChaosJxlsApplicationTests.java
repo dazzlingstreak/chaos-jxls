23e749b45db07130f41b012008c454debcb2b53f
huangdawei <dazzlingstreak@hotmail.com>
2020-05-26 16:06:41 +0800

    create project

---
+++ src/test/java/com/chaos/jxls/ChaosJxlsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chaos.jxls;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChaosJxlsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
